7b81389a0f2203daeaed79be96f0fb633901f728
Lawrence <pokepro95@gmail.com>
2022-07-05 22:33:28 -0700

    Complete exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(string, repetitions) {
+    if (repetitions < 0) return "ERROR";
+    let text = "";
+    for (let i = 0; i < repetitions; i++){
+        text += string;
+    }
+    return text;
 };
 
 // Do not edit below this line
