0e3898d0e501057ea6b265cc952b6787bdfb4937
Lawrence <pokepro95@gmail.com>
2022-07-07 00:40:29 -0700

    Complete exercise 6

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,9 @@
-const leapYears = function() {
-
+const leapYears = function(theYear) {
+    if (theYear % 4 === 0){
+        if (theYear % 100 === 0 && !(theYear % 400 === 0)) return false;
+        return true;
+    }
+    return false;
 };
 
 // Do not edit below this line
