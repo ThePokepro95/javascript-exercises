11a413f3398f9bfb4f08f9f662f01fb38d1ab057
Lawrence <pokepro95@gmail.com>
2022-07-07 00:12:34 -0700

    Complete exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,17 @@
 const removeFromArray = function(array, ...toRemove) {
-    
+    let arrayToReturn = [];
+    let addToArray = true;
+    for (let i = 0; i < array.length; i++){
+        addToArray = true;
+        for (let r = 0; r < toRemove.length; r++){
+            if (array[i]===toRemove[r]){
+                addToArray = false;
+                break;
+            }
+        }
+        if (addToArray) arrayToReturn.push(array[i]);
+    }
+    return arrayToReturn;
 };
 
 // Do not edit below this line
