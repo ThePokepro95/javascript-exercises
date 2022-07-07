cb8451300921857950047da1df430000451c1907
Lawrence <pokepro95@gmail.com>
2022-07-07 00:23:15 -0700

    Complete exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
-
+const sumAll = function(n1, n2) {
+    if (!(typeof n1 === "number" && typeof n2 ===  "number")) return "ERROR";
+    if (n1 < 0 || n2 < 0) return "ERROR";
+    let greater = n1;
+    let lesser = n2;
+    if (n2 > n1){
+        greater = n2;
+        lesser = n1;
+    }
+    let total = 0;
+    for (let i = lesser; i <= greater; i++){
+        total += i;
+    }
+    return total;
 };
 
 // Do not edit below this line
